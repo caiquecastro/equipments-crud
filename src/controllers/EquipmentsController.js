88d127b33a3b3d080701e5fcaad1340c42431f6f
Caíque de Castro Soares da Silva <castro.caique@gmail.com>
2020-05-16 15:53:07 -0300

    Add missing CRUD routes for equipments (#4)

--- src/controllers/EquipmentsController.js
+++ src/controllers/EquipmentsController.js
@@ -12,9 +12,49 @@ exports.index = async function index(req, res, next) {
 
 exports.store = async function store(req, res, next) {
   try {
-    const equipments = await Equipment.query();
+    const equipment = await Equipment.query().insert(req.body);
 
-    res.json(equipments);
+    res.status(201).json(equipment);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.show = async function show(req, res, next) {
+  try {
+    const equipment = await Equipment.query()
+      .findById(req.params.id)
+      .throwIfNotFound();
+
+    res.json(equipment);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.update = async function update(req, res, next) {
+  try {
+    const equipment = await Equipment.query()
+      .findById(req.params.id)
+      .throwIfNotFound();
+
+    await equipment.$query().patchAndFetch(req.body);
+
+    res.json(equipment);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.destroy = async function destroy(req, res, next) {
+  try {
+    const equipment = await Equipment.query()
+      .findById(req.params.id)
+      .throwIfNotFound();
+
+    await equipment.$query().delete();
+
+    res.status(204).send({});
   } catch (err) {
     next(err);
   }
--- src/routes/equipments.js
+++ src/routes/equipments.js
@@ -5,5 +5,9 @@ const router = express.Router();
 
 /* GET equipments listing. */
 router.get('/', EquipmentsController.index);
+router.post('/', EquipmentsController.store);
+router.get('/:id', EquipmentsController.show);
+router.patch('/:id', EquipmentsController.update);
+router.delete('/:id', EquipmentsController.destroy);
 
 module.exports = router;
