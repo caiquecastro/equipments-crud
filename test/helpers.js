e22350df8d03fb559843ed429ab4fa44fe583b87
Caique Castro <castro.caique@gmail.com>
2020-05-17 07:58:54 -0300

    Add dockerfile to build docker image

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,6 @@
 const Equipment = require('../src/models/Equipment');
 
-exports.createEquipment = async (rawAttributes = {}) => {
+exports.createEquipment = async function createEquipment(rawAttributes = {}) {
   return Equipment.query().insert({
     model: 'Model',
     category: 'toner',
