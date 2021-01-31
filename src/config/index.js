362c44276a4d5875a8cb5fdc598c54506593318a
Caique Silva <caique.castro@venturus.org.br>
2021-01-31 10:42:33 -0300

    fix: Fix lint

--- src/config/index.js
+++ src/config/index.js
@@ -4,8 +4,8 @@ module.exports = {
     connection: {
       host: process.env.DATABASE_HOST,
       user: process.env.DATABASE_USER,
-      password : process.env.DATABASE_PASSWORD,
-      database : process.env.DATABASE_NAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
     },
     useNullAsDefault: true,
     migrations: {
